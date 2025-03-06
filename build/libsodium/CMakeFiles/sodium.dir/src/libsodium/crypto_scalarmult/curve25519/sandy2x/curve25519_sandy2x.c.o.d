2e56842a18430c0cc79f5ebf520e4cf227dba8a8
d3a-n <toprakkilic2007@gmail.com>
2025-03-06 15:49:23 +0300

    Remove libsodium references

--- build/libsodium/CMakeFiles/sodium.dir/src/libsodium/crypto_scalarmult/curve25519/sandy2x/curve25519_sandy2x.c.o.d
+++
@@ -1,4 +0,0 @@
-libsodium/CMakeFiles/sodium.dir/src/libsodium/crypto_scalarmult/curve25519/sandy2x/curve25519_sandy2x.c.o: \
- /mnt/d5c8a1fe-f6d7-410c-bfca-89a411c4d388/Server/gitpear/libsodium/src/libsodium/crypto_scalarmult/curve25519/sandy2x/curve25519_sandy2x.c \
- /usr/include/stdc-predef.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
